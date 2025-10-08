0d3477ed9f96087e18cce0c62442a2617bf37f47
Debanjan Roy <debanjanroy123456@gmail.com>
2025-10-08 08:25:56 +0530

    Add detailed comments explaining the differences between .txt and .js files, and provide an overview of Node.js and Deno.js

--- javascript_basic/test.js
+++ javascript_basic/test.js
@@ -1,1 +1,17 @@
 console.log("Debanjan Roy")
+
+//difference between .txt and .js file 
+
+//A .txt file is a plain text file used to store simple, unformatted information such as notes, data, or documentation. It cannot be executed by a computer and is mainly opened using text editors like Notepad or TextEdit. In contrast, a .js file is a JavaScript file that contains executable code used to add interactivity, logic, and functionality to web pages. JavaScript files are run by web browsers or environments like Node.js. While a .txt file only holds readable text, a .js file holds code that can perform tasks, manipulate web content, and enhance user experience on websites.
+
+//environments .
+
+//node js.
+//Deno js .
+
+// ✅ Node.js was created by Ryan Dahl in 2009.
+// He developed it using Google’s V8 JavaScript engine to allow developers to run JavaScript code outside the browser, mainly for building fast and scalable server-side applications.
+
+// ✅ Deno.js was also created by Ryan Dahl in 2018.
+// It was built as a modern, secure replacement for Node.js — addressing some of Node’s design issues, like security and module management, and supports TypeScript by default.
+
